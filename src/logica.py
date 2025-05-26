a089312886fcdcb41b00567b6cc0741f1c7a0c22
Sxg0673 <santiago.castillo@uao.edu.co>
2025-05-25 23:55:00 -0500

    Agrega estructura de logica.py

---
+++ src/logica.py
@@ -0,0 +1,20 @@
+import random
+import csv
+import math
+import os
+import pandas as pd
+
+# Ruta del archivo CSV que almacenará los proyectos
+ARCHIVO_CSV = 'data/participantes.csv'
+
+def registrar_participante(nombre, r_resistencia, r_fuerza, r_velocidad):
+    pass
+
+def reporte_general():
+    pass
+
+def reporte_individual(nombre):
+    pass
+    
+def salir():
+    pass
